1b8d548f655fe41ccd0cd947b25a53239d8f8b82
Jalu <jalurumekso@gmail.com>
2019-09-02 16:13:53 +0700

    order-preview complete

--- application/views/dashboard/order/order-preview.php
+++ application/views/dashboard/order/order-preview.php
@@ -8,7 +8,7 @@
 
     <div class="col-9">
 
-      <div class="card shadow">
+      <div class="card shadow mb-4">
 
         <div class="col bg-primary text-white py-3 mb-4" style="border-top-left-radius: 0.35rem;border-top-right-radius: 0.35rem">
 
@@ -33,7 +33,23 @@
                     Judul:
                   </th>
                   <td>
-                    <span>Judul salah diketik dari github</span>
+                    <span>Badge SMAN 1 Lumajang</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="col" class="text-right">
+                    Barang:
+                  </th>
+                  <td>
+                    <span>Potongan Kain</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th scope="col" class="text-right">
+                    Bahan:
+                  </th>
+                  <td>
+                    <span>Katun</span>
                   </td>
                 </tr>
                 <tr>
@@ -41,7 +57,7 @@
                     Dimensi:
                   </th>
                   <td>
-                    <span>6x6cm</span>
+                    <span>4x4cm</span>
                   </td>
                 </tr>
                 <tr>
@@ -52,10 +68,95 @@
                     <span>merah, kuning, hijau</span>
                   </td>
                 </tr>
+                <tr>
+                  <th scope=" col" class="text-right">
+                    Ulang:
+                  </th>
+                  <td>
+                    <span>100x</span>
+                  </td>
+                </tr>
               </tbody>
             </table>
           </div>
 
+        </div>
+
+        <div class="row py-4">
+          <div class="col d-flex justify-content-center align-items-center">
+            <h5>"Layarnya hadap ke kanan dan tidak perlu diberi outline."</h5>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="row">
+
+    <div class="col-9">
+
+      <div class="card shadow mb-4">
+
+        <div class="col bg-primary text-white py-3 mb-4" style="border-top-left-radius: 0.35rem;border-top-right-radius: 0.35rem">
+
+          <div class="col d-flex justify-content-between align-items-center">
+            <h4 class="text-uppercase font-weight-bold my-0">Swasti Bordir</h4>
+            <p class="my-0">Formulir Desain Bordir</p>
+          </div>
+
+        </div>
+
+        <div class="row py-4">
+          <div class="col d-flex justify-content-center align-items-center">
+            <img class="img-fluid" src="<?= base_url('assets/img/artwork/') . 'summer-3658073_1280.png'; ?>" alt="">
+          </div>
+        </div>
+
+        <div class="row py-4">
+
+          <div class="col d-flex justify-content-center">
+            <ul class="list-group">
+              <li class="list-group-item">
+                <p class="my-0"><small>Judul:</small></p>
+                <p class="my-0" style="color:black">Logo Drumband SMAN 1 Lumajang</p>
+              </li>
+              <li class="list-group-item">
+                <p class="my-0"><small>Barang:</small></p>
+                <p class="my-0" style="color:black">Bendera</p>
+              </li>
+              <li class="list-group-item">
+                <p class="my-0"><small>Bahan:</small></p>
+                <p class="my-0" style="color:black">Katun</p>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col d-flex justify-content-center">
+            <ul class="list-group">
+              <li class="list-group-item">
+                <p class="my-0"><small>Dimensi:</small></p>
+                <p class="my-0" style="color:black">40x40cm</p>
+              </li>
+              <li class="list-group-item">
+                <p class="my-0"><small>Warna:</small></p>
+                <p class="my-0" style="color:black">Merah, kuning, hijau</p>
+              </li>
+              <li class="list-group-item">
+                <p class="my-0"><small>Ulang:</small></p>
+                <p class="my-0" style="color:black">1x</p>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+
+        <div class="row py-4">
+          <div class="col d-flex justify-content-center align-items-center">
+            <h5>"Layarnya hadap ke kanan dan tidak perlu diberi outline."</h5>
+          </div>
         </div>
 
       </div>
