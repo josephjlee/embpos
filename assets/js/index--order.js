545bf8acb290aaf37272344ee2ce5abc8bb1f2fd
Jalu <jalurumekso@gmail.com>
2019-09-06 23:05:58 +0700

    add a dynamic production config form

--- assets/js/index--order.js
+++ assets/js/index--order.js
@@ -45,4 +45,63 @@ $(document).ready(function () {
 		}
 	});
 
+
+	const uniqueFormWrapper = $('#spec-modal .modal-body #unique-form-wrapper');
+
+	const materialFormEl = `
+			<div class="form-group">
+				<label for="thread"><small>Benang</small></label>
+				<input type="text" name="production_material[thread]" id="thread" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="stabilizer"><small>Kain Keras</small></label>
+				<input type="text" name="production_material[stabilizer]" id="stabilizer" class="form-control">
+			</div>
+	`;
+
+	const designFormEl = `
+			<div class="form-group">
+				<label for="repeat"><small>Ulang</small></label>
+				<input type="text" name="production_design[repeat]" id="repeat" class="form-control">
+			</div>
+		`;
+
+	const embroFormEl = `
+			<div class="form-group">
+				<label for="otomatis"><small>Otomatis</small></label>
+				<input type="text" name="production_embro[otomatis]" id="otomatis" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="harga"><small>Harga</small></label>
+				<input type="text" name="production_embro[harga]" id="harga" class="form-control">
+			</div>
+	`;
+
+	$('#production-type').change(function (e) {
+
+		// Grab form processor url from data attribute on each select box option
+		let formAction = $(this).find('option:selected').data('form-action');
+
+		// Assign form url to form action
+		$(this).parents('form').attr('action', formAction);
+
+		// Populate form input element based on production type change
+		switch ($(this).val()) {
+
+			case 'material':
+				uniqueFormWrapper.html(materialFormEl);
+				break;
+
+			case 'design':
+				uniqueFormWrapper.html(designFormEl);
+				break;
+
+			case 'embroidery':
+				uniqueFormWrapper.html(embroFormEl);
+				$('#spec-modal #harga').val($('#spec-modal #order-price').val());
+				break;
+		}
+
+	});
+
 });
