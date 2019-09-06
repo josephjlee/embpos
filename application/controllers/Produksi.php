3c1ac61e874e3c9a0241e7288667d718b8bd0ac1
Jalu <jalurumekso@gmail.com>
2019-09-06 22:50:00 +0700

    adding daftar_desain and detail_desain method

--- application/controllers/Produksi.php
+++ application/controllers/Produksi.php
@@ -7,14 +7,38 @@ class Produksi extends CI_Controller {
     {
         parent::__construct();
 
-        $this->load->model('Produksi_model', 'produksi');
+        $this->load->model('produksi_model');
+    }
+
+    public function daftar_desain()
+    {
+        $data['title'] = 'Daftar Desain';
+
+        $data['design_list'] = $this->produksi_model->get_design_list();
+
+        $data['view_script'] = 'index--design.js';
+
+        $data['content'] = $this->load->view('dashboard/production/design-index', $data, TRUE);
+
+        $this->load->view('layout/dashboard', $data);
+    }
+
+    public function detail_desain($production_design_id)
+    {
+        $data['title'] = "Pesanan Desain: DSN-{$production_design_id}";
+
+        $data['design_detail'] = $this->produksi_model->get_design_detail_by_id($production_design_id);
+
+        $data['content'] = $this->load->view('dashboard/production/design-detail', $data, TRUE);
+
+        $this->load->view('layout/dashboard', $data);
     }
 
     public function proses()
     {
         $data['title'] = 'Proses Berjalan';
 
-        $data['production_data'] = $this->produksi->get_production_card_data();
+        $data['production_data'] = $this->produksi_model->get_production_card_data();
         $data['content'] = $this->load->view('dashboard/production/production--checklist', $data, TRUE);
         
         $this->load->view('layout/dashboard', $data);
