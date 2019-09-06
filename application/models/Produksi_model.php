a629db378fcc0bec2301072aabc13c69371ac62a
Jalu <jalurumekso@gmail.com>
2019-09-06 22:51:58 +0700

    adding method for design related data

--- application/models/Produksi_model.php
+++ application/models/Produksi_model.php
@@ -2,6 +2,71 @@
 
 class Produksi_model extends CI_Model
 {
+    public function pesan_desain($order)
+    {
+        $design_data = $this->siapkan_data($order);
+
+        $this->db->insert('production_design', $design_data);
+    }
+
+    public function siapkan_data($order)
+    {
+
+        $design_db_data = [];
+
+        foreach ($order as $col => $val) {
+            $design_db_data[$col] = $val;
+        }
+
+        return $design_db_data;
+    }
+
+    public function get_design_list()
+    {
+        $this->db->select('
+            order.machine_file as emb,
+            order.image as artwork,
+            order.number as order_number,
+            order.description as title,
+            order.dimension,
+            order.material,
+            order.color,
+            order.required_date as required,
+            production_design.production_design_id as id,
+            production_design.repeat,
+            work_status.name as status
+        ');
+        $this->db->from('production_design');
+        $this->db->join('order', 'production_design.order_id = order.order_id');
+        $this->db->join('work_status', 'production_design.work_status_id = work_status.work_status_id');
+
+        return $this->db->get()->result_array();
+    }
+    
+    public function get_design_detail_by_id($production_design_id)
+    {
+        $this->db->select('
+            order.image as artwork,
+            order.description as title,
+            order.dimension,
+            order.material,
+            order.color,
+            order.required_date as required,
+            order.note,
+            production_design.production_design_id as id,
+            production_design.repeat,
+            production_design.color_order,
+            item.name as item,
+            work_status.name as status
+        ');
+        $this->db->from('production_design');
+        $this->db->join('order', 'production_design.order_id = order.order_id');
+        $this->db->join('item', 'order.item_id = item.item_id');
+        $this->db->join('work_status', 'production_design.work_status_id = work_status.work_status_id');
+        $this->db->where('production_design_id', $production_design_id);
+
+        return $this->db->get()->row_array();
+    }
 
     public function check_process_icon($date_started, $date_finished)
     {
